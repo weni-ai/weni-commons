352c548a6b5ccc403635f5388fbcea1d3799b5ef
RuanHeleno <ruan.empreenderop@gmail.com>
2026-08-14 09:50:29 -0300

    chore: bump project version to 1.4.2a6 and add new entity and module types
    - Updated version in pyproject.toml to 1.4.2a6.
    - Added AGENT to Entity enum.
    - Added KNOWLEDGE_BASE, INSTRUCTIONS, and MY_AGENTS to Module enum.

--- weni_commons/change_history/modules.py
+++ weni_commons/change_history/modules.py
@@ -6,6 +6,10 @@ class Module(str, Enum):
     LIVE_DESK = "LIVE_DESK"
     NEXUS = "NEXUS"
 
+    KNOWLEDGE_BASE = "KNOWLEDGE_BASE"
+    INSTRUCTIONS = "INSTRUCTIONS"
+    MY_AGENTS = "MY_AGENTS"
+
     @classmethod
     def to_choices(cls):
         return [(module.value, module.value) for module in cls]
